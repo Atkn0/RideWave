462815ccff0df2a3edff3d5239317161a17b951a
BCaganDemir <berkecagandemir@ogr.eskisehir.edu.tr>
2023-12-13 19:23:02 +0300

    sa as

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         loginPageFrom form = new loginPageFrom();
         form.setVisible(true);
+        System.out.println("Sa as");
     }
 }
