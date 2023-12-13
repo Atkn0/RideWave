716a3bc1978115f5e0e77076ed635e78b66dae0b
Atkn0 <a.atakantosun@gmail.com>
2023-12-13 18:55:57 +0300

    first loginPage commit

--- src/loginPageFrom.java
+++ src/loginPageFrom.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class loginPageFrom {
+import javax.swing.*;
+
+public class loginPageFrom extends JFrame {
+
+    JPanel panel1;
+    private JTextField textField1;
+    private JPasswordField passwordField1;
+    private JButton loginButton;
+
+    public loginPageFrom(){
+        add(panel1);
+        setSize(400,400);
+        setTitle("RideWave Login");
+
+        loginButton.addActionListener(e -> {
+            String username = textField1.getText();
+            String password = passwordField1.getText();
+
+            if(username.equals("admin") && password.equals("admin")){
+                JOptionPane.showMessageDialog(null,"Login Successful");
+            }
+            else{
+                JOptionPane.showMessageDialog(null,"Login Failed");
+            }
+        });
+
+
+    }
+
 }
