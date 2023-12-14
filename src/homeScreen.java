7d6ded2260fefee4e4f47f73486aa325c71e198c
BCaganDemir <berkecagandemir@ogr.eskisehir.edu.tr>
2023-12-14 20:47:06 +0300

    screen

--- src/homeScreen.java
+++ src/homeScreen.java
@@ -1,2 +1,21 @@
-public class homeScreen {
+import javax.swing.JFrame;
+import java.awt.*;
+
+public class homeScreen extends JFrame {
+    public homeScreen() {
+        // JFrame özelliklerini ayarlamak
+        setTitle("Ana Sayfa");
+        setSize(800, 600); // Genişlik ve yükseklik
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null); // Pencereyi ekranın ortasına yerleştirme
+        setLayout(new BorderLayout()); // Layout yöneticisini belirleme (isteğe bağlı)
+
+
+    }
+
+    public static void main(String[] args) {
+
+        homeScreen anaSayfa = new homeScreen();
+        anaSayfa.setVisible(true);
+    }
 }
